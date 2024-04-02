4d568e0e19b43c84ccb28f08eb030386fb10ac7b
Vitalii Sliusar <sliusardev@gmail.com>
2024-04-02 23:36:26 +0300

    updated and added medical theme

---
+++ resources/views/themes/medical/index.blade.php
@@ -0,0 +1,9 @@
+@extends('themes.medical.layouts.app', [
+    'sidebar' => false,
+    ])
+
+@section('content')
+
+    @include('themes.medical.partials.home')
+
+@endsection
---
+++ resources/views/themes/medical/partials/home.blade.php
@@ -0,0 +1,236 @@
+<div class="banner-wrap">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <span class="bunner-sub-title"># Skin Treatment</span>
+                <div class="bunner-title">Every Day Take Care In Your Own Skin</div>
+                <div class="bunner-descriptions">No matter what your skin concerns we have an experienced professional doctor to give you the best results for your skin.</div>
+                <a href="#" class="bunner-link">View Our Services</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="service-wrap">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4">
+                <div class="service-item">
+                    <a href="#" class="service-item-wrap">
+                        <div class="service-img-wrap">
+                            <img src="img/microscope.png" alt="" />
+                        </div>
+                        <span class="service-item-title"> Safety & Technology </span>
+                    </a>
+                    <span class="service-item-description">Proin eget tortor risus vivamus suscipit tortor eget felis porttitor volutpat cras ultricies ligula sed magna dictum porta.</span>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="service-item">
+                    <a href="#" class="service-item-wrap">
+                        <div class="service-img-wrap">
+                            <img src="img/medical-doctor.png" alt="" />
+                        </div>
+                        <span class="service-item-title">   Experienced Team </span>
+                    </a>
+                    <span class="service-item-description">Proin eget tortor risus vivamus suscipit tortor eget felis porttitor volutpat cras ultricies ligula sed magna dictum porta.</span>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="service-item">
+                    <a href="#" class="service-item-wrap">
+                        <div class="service-img-wrap">
+                            <img src="img/medical-tool.png" alt="" />
+                        </div>
+                        <span class="service-item-title">  Modern Equipment  </span>
+                    </a>
+                    <span class="service-item-description">Proin eget tortor risus vivamus suscipit tortor eget felis porttitor volutpat cras ultricies ligula sed magna dictum porta.</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="about-wrap">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="about-img-wrap">
+                    <img src="img/about.jpg" alt="" />
+                    <div class="skin-care-about-wrap">
+                        <div class="skin-wrap">
+                            <div class="skin-care-about-item">
+                                <span class="care-about-item-number">20+</span>
+                                <span class="care-about-item-title">Years of Experience</span>
+                            </div>
+                            <div class="skin-care-about-item">
+                                <span class="care-about-item-number">2K</span>
+                                <span class="care-about-item-title">Customers Treated</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="about-info-wrap">
+                    <span class="about-sub-title"># About Treatment</span>
+                    <div class="about-title">We Carefully Manage The Treatment Of Our Patients In The Clinical Environment</div>
+                    <div class="about-descriptions">Vivamus magna justo lacinia eget consectetur sed convallis at tellus. Nulla quis lorem ut libero malesuada feugiat. Curabitur aliquet quam id dui posuere blandit. Quisque velit nisi pretium ut lacinia in elementum id enim. Donec sollicitudin molestie malesuada. Curabitur arcu erat accumsan id imperdiet et porttitor at sem.</div>
+                    <a href="#" class="about-link">Learn More</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="popular-services">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <span class="popular-services-sub"># Skin Care Treatments </span>
+                <span class="popular-services-title">Our Most Popular Services </span>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-4">
+                <div class="popular-services-item">
+                    <a href="#" class="popular-services-item-wrap">
+                        <div class="popular-services-img-wrap">
+                            <img src="img/services-1.jpg" alt="" />
+                        </div>
+                        <div class="popular-services-wrap-text">
+                            <span class="popular-services-item-title"> Skin Care </span>
+                            <span class="popular-services-item-description">Proin eget tortor risus vivamus suscipit tortor eget felis porttitor volutpat cras ultricies ligula sed magna dictum porta.</span>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="popular-services-item">
+                    <a href="#" class="popular-services-item-wrap">
+                        <div class="popular-services-img-wrap">
+                            <img src="img/services-2.jpg" alt="" />
+                        </div>
+                        <div class="popular-services-wrap-text">
+                            <span class="popular-services-item-title"> Needle Mesotherapy </span>
+                            <span class="popular-services-item-description">Proin eget tortor risus vivamus suscipit tortor eget felis porttitor volutpat cras ultricies ligula sed magna dictum porta.</span>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="popular-services-item">
+                    <a href="#" class="popular-services-item-wrap">
+                        <div class="popular-services-img-wrap">
+                            <img src="img/services-3.jpg" alt="" />
+                        </div>
+                        <div class="popular-services-wrap-text">
+                            <span class="popular-services-item-title">  Hair Removal </span>
+                            <span class="popular-services-item-description">Proin eget tortor risus vivamus suscipit tortor eget felis porttitor volutpat cras ultricies ligula sed magna dictum porta.</span>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="popular-services-item">
+                    <a href="#" class="popular-services-item-wrap">
+                        <div class="popular-services-img-wrap">
+                            <img src="img/services-4.jpg" alt="" />
+                        </div>
+                        <div class="popular-services-wrap-text">
+                            <span class="popular-services-item-title">  Laser Treatments  </span>
+                            <span class="popular-services-item-description">Proin eget tortor risus vivamus suscipit tortor eget felis porttitor volutpat cras ultricies ligula sed magna dictum porta.</span>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="popular-services-item">
+                    <a href="#" class="popular-services-item-wrap">
+                        <div class="popular-services-img-wrap">
+                            <img src="img/services-5.jpg" alt="" />
+                        </div>
+                        <div class="popular-services-wrap-text">
+                            <span class="popular-services-item-title"> Skin Resurfacing </span>
+                            <span class="popular-services-item-description">Proin eget tortor risus vivamus suscipit tortor eget felis porttitor volutpat cras ultricies ligula sed magna dictum porta.</span>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="popular-services-item">
+                    <a href="#" class="popular-services-item-wrap">
+                        <div class="popular-services-img-wrap">
+                            <img src="img/services-6.jpg" alt="" />
+                        </div>
+                        <div class="popular-services-wrap-text">
+                            <span class="popular-services-item-title">  Plastic Surgery </span>
+                            <span class="popular-services-item-description">Proin eget tortor risus vivamus suscipit tortor eget felis porttitor volutpat cras ultricies ligula sed magna dictum porta.</span>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="blog">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <span class="blog-sub"># Update News & Blog </span>
+                <span class="blog-title">Stay Updated With Our Latest News And Blog Articles </span>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-4">
+                <div class="blog-item">
+                    <a href="#" class="blog-item-wrap">
+                        <div class="blog-img-wrap">
+                            <img src="img/blog-1.jpg" alt="" />
+                        </div>
+                        <div class="blog-wrap-info">
+                            <span class="blog-item-info"><i class="fal fa-user"></i>By: Medical cms</span>
+                            <span class="blog-item-info"><i class="far fa-clock"></i>30 Dec, 2024</span>
+                        </div>
+                        <div class="blog-wrap-text">
+                            <span class="blog-item-title">Discover The Confidence Of Your Skin Treatment </span>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="blog-item">
+                    <a href="#" class="blog-item-wrap">
+                        <div class="blog-img-wrap">
+                            <img src="img/blog-2.jpg" alt="" />
+                        </div>
+                        <div class="blog-wrap-info">
+                            <span class="blog-item-info"><i class="fal fa-user"></i>By: Medical cms</span>
+                            <span class="blog-item-info"><i class="far fa-clock"></i>30 Dec, 2024</span>
+                        </div>
+                        <div class="blog-wrap-text">
+                            <span class="blog-item-title">Everyone Needs To Know The Tips To Get Rid Of Acne Face</span>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="blog-item">
+                    <a href="#" class="blog-item-wrap">
+                        <div class="blog-img-wrap">
+                            <img src="img/blog-3.jpg" alt="" />
+                        </div>
+                        <div class="blog-wrap-info">
+                            <span class="blog-item-info"><i class="fal fa-user"></i>By: Medical cms</span>
+                            <span class="blog-item-info"><i class="far fa-clock"></i>30 Dec, 2024</span>
+                        </div>
+                        <div class="blog-wrap-text">
+                            <span class="blog-item-title">Can You Solve Any Skin Damage Problem?</span>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
