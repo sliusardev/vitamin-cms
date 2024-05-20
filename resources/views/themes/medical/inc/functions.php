71d0b9cc72022b5fe401ad9ed267eb7bf6a8324a
sliusardev <sliusardev@gmail.com>
2024-05-20 22:46:23 +0300

    added doctors custom field

--- resources/views/themes/medical/inc/functions.php
+++ resources/views/themes/medical/inc/functions.php
@@ -1,12 +1,19 @@
 <?php
 
 use App\Services\CustomFieldService;
-use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\FileUpload;
 use Filament\Forms\Components\Repeater;
-use Filament\Forms\Components\RichEditor;
-use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
-CustomFieldService::setCustomFields('about', [
-    RichEditor::make('custom_fields.education')
-        ->columnSpan('full'),
+
+CustomFieldService::setCustomFields('doctors', [
+    Repeater::make('doctors')->schema([
+        TextInput::make('Name')->required(),
+        TextInput::make('Role')->required(),
+        FileUpload::make('thumb')
+            ->label(trans('dashboard.thumb'))
+            ->directory('images')
+            ->imageEditor()
+            ->image()
+            ->columnSpanFull(),
+    ])->columns(2)
 ]);
