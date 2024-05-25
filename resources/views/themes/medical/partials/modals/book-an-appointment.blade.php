675f791251a2f3beb38d3e2fbee8a13570b1cbb6
sliusardev <sliusardev@gmail.com>
2024-05-25 12:51:46 +0300

    added bookAnAppointment modal fields

--- resources/views/themes/medical/partials/modals/book-an-appointment.blade.php
+++ resources/views/themes/medical/partials/modals/book-an-appointment.blade.php
@@ -8,20 +8,29 @@
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-                <form action="">
+                <form action="{{formActionSlug('book-an-appointment')}}" method="post">
+                    @csrf
                     <div class="mb-3">
-                        <label for="email" class="form-label">Email address</label>
-                        <input type="email" name="email" class="form-control" id="email" placeholder="name@example.com">
+                        <label for="appointment-form--name" class="form-label">Name</label>
+                        <input type="text" name="name" class="form-control" id="appointment-form-name" placeholder="Name">
                     </div>
                     <div class="mb-3">
-                        <label for="phone" class="form-label">Phone</label>
-                        <input type="text" name="phone" class="form-control" id="phone" placeholder="+13245345">
+                        <label for="appointment-form-email" class="form-label">Email address</label>
+                        <input type="email" name="email" class="form-control" id="appointment-form-email" placeholder="name@example.com">
                     </div>
                     <div class="mb-3">
-                        <label for="message" class="form-label">Example textarea</label>
-                        <textarea class="form-control" name="message" id="message" rows="3"></textarea>
+                        <label for="appointment-form-phone" class="form-label">Phone</label>
+                        <input type="text" name="phone" class="form-control" id="appointment-form-phone" placeholder="+13245345">
                     </div>
-                    <button type="submit" class="btn btn-secondary w-100">Send</button>
+                    <div class="mb-3">
+                        <label for="appointment-form-datetime" class="form-label">Date and Time</label>
+                        <input type="datetime-local" name="datetime" class="form-control" id="appointment-form-datetime">
+                    </div>
+                    <div class="mb-3">
+                        <label for="appointment-form-message" class="form-label">Additional message</label>
+                        <textarea class="form-control" name="message" id="appointment-form-message" rows="3"></textarea>
+                    </div>
+                    <button type="submit" class="btn btn-secondary w-100 medical-bg">Send</button>
                 </form>
             </div>
         </div>
