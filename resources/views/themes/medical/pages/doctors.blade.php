9f2e7256c6a8ae8bf9dd17b9f008e4b4de8fdb51
Maks <maksimjurba90210@gmail.com>
2024-05-25 23:52:02 +0300

    add template doctors add images css

--- resources/views/themes/medical/pages/doctors.blade.php
+++ resources/views/themes/medical/pages/doctors.blade.php
@@ -21,10 +21,87 @@
             </div>
         </div>
     </div>
-    <div class="contact-page-info-wrap">
+    <div class="doctors-page-info-wrap">
         <div class="container">
             <div class="row">
-
+                <div class="col-lg-4">
+                    <div class="doctor-wrap">
+                        <img src="{{asset('themes/medical/img/doctor-1.jpg')}}" alt="" />
+                        <span class="doctor-name">Dr. Johnson Melbourne </span>
+                        <span class="doctor-positions">Prosthodontics Dentist </span>
+                        <div class="doctor-socials">
+                            <a href="#"><i class="fab fa-facebook-f"></i></a>
+                            <a href="#"><i class="fab fa-twitter"></i></a>
+                            <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                            <a href="#"><i class="fab fa-instagram"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="doctor-wrap">
+                        <img src="{{asset('themes/medical/img/doctor-2.jpg')}}" alt="" />
+                        <span class="doctor-name">Dr. Ena Dicrosa</span>
+                        <span class="doctor-positions">Aesthetic Dentistry</span>
+                        <div class="doctor-socials">
+                            <a href="#"><i class="fab fa-facebook-f"></i></a>
+                            <a href="#"><i class="fab fa-twitter"></i></a>
+                            <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                            <a href="#"><i class="fab fa-instagram"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="doctor-wrap">
+                        <img src="{{asset('themes/medical/img/doctor-3.jpg')}}" alt="" />
+                        <span class="doctor-name">Dr. Addison Smith</span>
+                        <span class="doctor-positions">Gastroenterologists</span>
+                        <div class="doctor-socials">
+                            <a href="#"><i class="fab fa-facebook-f"></i></a>
+                            <a href="#"><i class="fab fa-twitter"></i></a>
+                            <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                            <a href="#"><i class="fab fa-instagram"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="doctor-wrap">
+                        <img src="{{asset('themes/medical/img/doctor-6.jpg')}}" alt="" />
+                        <span class="doctor-name">Dr. Edie Dee </span>
+                        <span class="doctor-positions">Cardiologists</span>
+                        <div class="doctor-socials">
+                            <a href="#"><i class="fab fa-facebook-f"></i></a>
+                            <a href="#"><i class="fab fa-twitter"></i></a>
+                            <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                            <a href="#"><i class="fab fa-instagram"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="doctor-wrap">
+                        <img src="{{asset('themes/medical/img/doctor-7.jpg')}}" alt="" />
+                        <span class="doctor-name">Dr. Sarah Taylor</span>
+                        <span class="doctor-positions">Prosthodontics Dentist</span>
+                        <div class="doctor-socials">
+                            <a href="#"><i class="fab fa-facebook-f"></i></a>
+                            <a href="#"><i class="fab fa-twitter"></i></a>
+                            <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                            <a href="#"><i class="fab fa-instagram"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="doctor-wrap">
+                        <img src="{{asset('themes/medical/img/doctor-8.jpg')}}" alt="" />
+                        <span class="doctor-name">Dr. Aiken Ward</span>
+                        <span class="doctor-positions">Aesthetic Dentistry</span>
+                        <div class="doctor-socials">
+                            <a href="#"><i class="fab fa-facebook-f"></i></a>
+                            <a href="#"><i class="fab fa-twitter"></i></a>
+                            <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                            <a href="#"><i class="fab fa-instagram"></i></a>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
