2b85c9195e7556729cffee8d8c0a747925bbdfd7
Maks <maksimjurba90210@gmail.com>
2024-05-27 18:07:38 +0300

    edit custom page - page.blade

--- resources/views/themes/medical/pages/page.blade.php
+++ resources/views/themes/medical/pages/page.blade.php
@@ -7,32 +7,32 @@
 
 @section('content')
 
-    <nav aria-label="breadcrumb">
-        <ol class="breadcrumb">
-            <li class="breadcrumb-item"><a href="{{route('home')}}">Головна</a></li>
-            <li class="breadcrumb-item active" aria-current="page">{{$page->title}}</li>
-        </ol>
-    </nav>
-
-    <div class="cs-post cs-style1 cs-type1">
-
-        @if(!empty($page->thumb()))
-            <div class="cs-post__thumb">
-                <div class="cs-post__thumb__in cs-bg" data-src="{{$page->thumb()}}"></div>
-            </div>
-        @endif
-
-        <div class="cs-post__info">
-            <div class="cs-post__meta cs-style1">
-                <a href="#" class="cs-post__author"><i class="fas fa-calendar"></i>{{$page->date()}}</a>
-                <div class="cs-post__view"><i class="fas fa-eye"></i>{{$page->views}}</div>
-            </div>
-            <h2 class="cs-post__title mb0">{{$page->title}}</h2>
-            <div class="cs-post__description">
-                {!! $page->content !!}
+    <div class="custom-page-title-wrap">
+        <div class="container">
+            <div class="row ai">
+                <div class="col-lg-12">
+                    <span class="contact-page-title">{{$page->title}}</span>
+                    <ul class="breadcrumbs">
+                        <li><a href="{{route('home')}}">Home</a></li>
+                        <span class="breadcrumbs-slash"> / </span>
+                        <li><span>{{$page->title}}</span></li>
+                    </ul>
+                </div>
             </div>
         </div>
     </div>
-    <div class="cs-height__80 cs-height__lg__40"></div>
+
+    <div class="page-single">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="page-single-img-wrap">
+                        <img src="{{$page->thumb()}}" alt="" />
+                    </div>
+                    <div class="page-single-content-wrap">{!! $page->content !!}</div>
+                </div>
+            </div>
+        </div>
+    </div>
 
 @endsection
